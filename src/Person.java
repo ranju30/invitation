6cc869ef4e642a01753cb11916e69924f54ee0e1
ranju <ranjukr@thoughtworks.com>
2016-03-03 19:59:25 +0530

    added getAge and getAddress

--- src/Person.java
+++ src/Person.java
@@ -8,18 +8,26 @@ public class Person {
 
     public Person(String firstName, String lastName, String gender, int age, String[] address) {
         this.firstName = firstName;
-        this.lastName =lastName;
+        this.lastName = lastName;
         this.gender = gender;
         this.age = age;
         this.address = address;
     }
 
-    public String getName(){
-        return firstName+" "+lastName;
+    public String getName() {
+        return firstName + " " + lastName;
     }
 
-    public String generatePrefix(){
+    public String generatePrefix() {
         return this.gender == "male" ? "Mr " : "Ms ";
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getAddress(){
+        return this.address[0];
     }
 
 }
--- test/PersonTest.java
+++ test/PersonTest.java
@@ -6,16 +6,28 @@ import static junit.framework.TestCase.assertEquals;
 public class PersonTest {
     @Test
     public void test_getName_will_give_the_name_of_the_person() throws Exception {
-        Person person = new Person("Ranju","Karmakar","male",22, new String[]{"kolkata WB India"});
+        Person person = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
         String name = person.getName();
-        assertEquals(name,"Ranju Karmakar");
+        assertEquals(name, "Ranju Karmakar");
     }
 
     @Test
     public void test_to_get_prefix_according_to_gender() throws Exception {
-        Person ranju = new Person("Ranju","Karmakar","male",22, new String[]{"kolkata WB India"});
-        Person girl = new Person("Shreya","Ghoshal","female",25, new String[]{"howrah WB India"});
-        assertEquals("Mr ",ranju.generatePrefix());
-        Assert.assertEquals("Ms ",girl.generatePrefix());
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
+        Person shreya = new Person("Shreya", "Ghoshal", "female", 25, new String[]{"howrah WB India"});
+        assertEquals("Mr ", ranju.generatePrefix());
+        assertEquals("Ms ", shreya.generatePrefix());
+    }
+
+    @Test
+    public void test_to_get_age_of_a_given_detail() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
+        Assert.assertEquals(22,ranju.getAge());
+    }
+
+    @Test
+    public void test_to_get_address_of_a_given_detail() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
+        assertEquals("kolkata WB India",ranju.getAddress());
     }
 }
