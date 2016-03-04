c17b3a27fc3f20b3c751fddbf189325c4da73d07
ranju <ranjukr@thoughtworks.com>
2016-03-04 09:20:24 +0530

    More parameter added to constructor getter function added

--- src/Person.java
+++ src/Person.java
@@ -4,33 +4,49 @@ public class Person {
     private String lastName;
     private String gender;
     private int age;
-    private String[] address;
+    private String city;
+    private String state;
+    private String country;
 
-    public Person(String firstName, String lastName, String gender, int age, String[] address) {
+    public Person(String firstName, String lastName, String gender, int age, String city, String state, String country) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
         this.age = age;
-        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.country = country;
     }
 
     public String getName() {
         return firstName + " " + lastName;
     }
 
-    public String generatePrefix() {
-        return this.gender.equals("male") ? "Mr " : "Ms ";
-    }
-
     public int getAge() {
         return this.age;
     }
 
-    public String getAddress() {
-        return this.address[0];
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public String generatePrefix() {
+        return this.gender.equals("male") ? "Mr " : "Ms ";
     }
 
     public String makeAddressing() {
         return generatePrefix() + getName();
+    }
+
+    public String getNameAndCountry() {
+        return makeAddressing() + "," + getCountry();
     }
 }
--- test/PersonTest.java
+++ test/PersonTest.java
@@ -5,35 +5,60 @@ import static junit.framework.TestCase.assertEquals;
 public class PersonTest {
     @Test
     public void test_getName_will_give_the_name_of_the_person() throws Exception {
-        Person person = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
+        Person person = new Person("Ranju", "Karmakar", "male", 22, "kolkata", "WB", "India");
         String name = person.getName();
         assertEquals(name, "Ranju Karmakar");
     }
 
     @Test
+    public void test_to_get_age_of_a_given_detail() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
+        assertEquals(22,ranju.getAge());
+    }
+
+    @Test
+    public void test_to_get_city_of_a_given_person() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
+        assertEquals("kolkata",ranju.getCity());
+    }
+
+    @Test
+    public void test_to_get_state_of_a_given_person() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
+        assertEquals("WB",ranju.getState());
+    }
+
+    @Test
+    public void test_to_get_country_of_a_given_detail() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
+        assertEquals("India",ranju.getCountry());
+    }
+
+    @Test
     public void test_to_get_prefix_according_to_gender() throws Exception {
-        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
-        Person shreya = new Person("Shreya", "Ghoshal", "female", 25, new String[]{"howrah WB India"});
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
+        Person shreya = new Person("Shreya", "Ghoshal", "female", 25, "howrah", "WB", "India");
         assertEquals("Mr ", ranju.generatePrefix());
         assertEquals("Ms ", shreya.generatePrefix());
     }
 
     @Test
-    public void test_to_get_age_of_a_given_detail() throws Exception {
-        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
-        assertEquals(22,ranju.getAge());
-    }
-
-    @Test
-    public void test_to_get_address_of_a_given_detail() throws Exception {
-        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
-        assertEquals("kolkata WB India",ranju.getAddress());
-    }
-
-    @Test
     public void test_to_get_name_and_prefix() throws Exception {
-        Person ranju = new Person("Ranju", "Karmakar", "male", 22, new String[]{"kolkata WB India"});
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
         assertEquals("Mr Ranju Karmakar",ranju.makeAddressing());
+    }
 
+    @Test
+    public void test_to_get_name_and_prefix_for_female() throws Exception {
+        Person shreya = new Person("Shreya", "Ghoshal", "female", 25, "howrah", "WB", "India");
+        assertEquals("Ms Shreya Ghoshal", shreya.makeAddressing());
+    }
+
+    @Test
+    public void test_to_get_name_and_country() throws Exception {
+        Person ranju = new Person("Ranju", "Karmakar", "male", 22,  "kolkata", "WB", "India");
+        Person shreya = new Person("Shreya", "Ghoshal", "female", 25, "howrah", "WB", "India");
+        assertEquals("Mr Ranju Karmakar,India", ranju.getNameAndCountry());
+        assertEquals("Ms Shreya Ghoshal,India", shreya.getNameAndCountry());
     }
 }
