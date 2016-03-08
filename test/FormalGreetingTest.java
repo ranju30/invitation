f0af818362119473492da43e314b1b715e12793a
ranju <ranjukr@thoughtworks.com>
2016-03-08 23:17:58 +0530

    Ranju - added test for representation
    What changes have you made?
    Added test for particular arguments.
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- test/FormalGreetingTest.java
+++ test/FormalGreetingTest.java
@@ -1,3 +1,16 @@
-public class FormalGreetingTest {
+import com.FormalGreeting;
+import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FormalGreetingTest {
+    @Test
+    public void test_to_get_the_detail_from_a_particular_file_according_to_the_inputs() throws Exception {
+        String[] args = {"./data/sample.csv","India","20"};
+        FormalGreeting formalGreeting = new FormalGreeting(args);
+        ArrayList<String> formalRepresentation = formalGreeting.getFormalRepresentation();
+        assertEquals("Mr Karmakar, Ranju, India, 22",formalRepresentation.get(0));
+    }
 }
--- test/InformalGreetingTest.java
+++ test/InformalGreetingTest.java
@@ -1,3 +1,16 @@
-public class InformalGreetingTest {
+import com.InformalGreeting;
+import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class InformalGreetingTest {
+    @Test
+    public void test_to_get_the_detail_from_a_particular_file_according_to_the_inputs() throws Exception {
+        String[] args = {"./data/sample.csv","India","20"};
+        InformalGreeting informalGreeting = new InformalGreeting(args);
+        ArrayList<String> informalRepresentation = informalGreeting.getInformalRepresentation();
+        assertEquals("Mr Ranju Karmakar, India, 22",informalRepresentation.get(0));
+    }
 }
